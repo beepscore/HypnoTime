5538de77af7f4cf15a3978a00b44e98b0363b12b
beepscore <support@beepscore.com>
2010-11-28 21:50:09 -0800

    Initial commit.  Ref Conway and Hillegass Ch 07.

---
+++ Classes/HypnoTimeAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HypnoTimeAppDelegate.h
+//  HypnoTime
+//
+//  Created by Steve Baker on 11/28/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoTimeAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
