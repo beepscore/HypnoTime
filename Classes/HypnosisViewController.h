4a82eb62e06d2e7d49ab7a0afe10b72019937bec
beepscore <support@beepscore.com>
2011-01-09 16:48:20 -0800

    Add files CurrentTimeViewController .h,.m, .xib and HypnosisViewController .h, .m

---
+++ Classes/HypnosisViewController.h
@@ -0,0 +1,16 @@
+//
+//  HypnosisViewController.h
+//  HypnoTime
+//
+//  Created by Steve Baker on 1/9/11.
+//  Copyright 2011 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HypnosisViewController : UIViewController {
+
+}
+
+@end
